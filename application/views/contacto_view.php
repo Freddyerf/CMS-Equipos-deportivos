b4d2d5da797c8339215c1bcf11301262d6734405
Freddy R <frondonfermin@gmail.com>
2017-12-04 19:24:15 -0400

    Modifiqué el encabezado y añadí la vista de contacto

---
+++ application/views/contacto_view.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<div class="container">
+    <div class="row">
+        <div class="col col-sm-8 col-sm-offset-2">
+            <div class="well well-sm">
+                <form class="form-horizontal" method="post">
+                    <fieldset>
+                        <legend class="text-center header">Contáctanos</legend>
+                        <div class="form-group">
+                            <span class="col-md-2 text-center"><i class="fa fa-user bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="fname" name="name" type="text" placeholder="Nombre" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <span class="col-md-2 text-center"><i class="fa fa-envelope-o bigicon"></i></span>
+                            <div class="col-md-8">
+                                <input id="email" name="email" type="text" placeholder="Correo electrónico" class="form-control">
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <span class="col-md-2 text-center"><i class="fa fa-pencil-square-o bigicon"></i></span>
+                            <div class="col-md-8">
+                                <textarea class="form-control" id="message" name="message" placeholder="Entra tu mensaje aquí." rows="7"></textarea>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-12 text-center">
+                                <button type="submit" class="btn btn-primary btn-lg">Submit</button>
+                            </div>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
--- application/views/plantilla/encabezado.php
+++ application/views/plantilla/encabezado.php
@@ -6,26 +6,34 @@
 		<style>
 			.centro{
 				min-height: 300px;
+				margin-top: 50px;
 			}
 		</style>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	</head>
 	<body>
 		<div class = "container">
-      <nav class="navbar navbar-inverse">
+      <nav class="navbar navbar-inverse navbar-fixed-top">
     	  <div class="container-fluid">
     		    <div class="navbar-header">
-    		        <a class="navbar-brand" href="#">Equipos deportivos</a>
+    		        <a class="active" href="#">Equipos deportivos</a>
     		    </div>
     		    <ul class="nav navbar-nav">
-        		  <li class="active"><a href="#">Home</a></li>
-        		  <li><a href="#">Page 1</a></li>
-        		  <li><a href="#">Page 2</a></li>
+        		  <li><a href="#">Inicio</a></li>
+        		  <li><a href="#">Noticias</a></li>
+        		  <li><a href="#">Galería</a></li>
+							<li><a href="#">Eventos</a></li>
+							<li><a href="#">Clasificados</a></li>
+							<li><a href="#">Contacto</a></li>
+							<li><a href="#">FAQ</a></li>
+							<li><a href="#">Admin</a></li>
     		    </ul>
     		    <ul class="nav navbar-nav navbar-right">
-        		  <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-        		  <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+							<li><p class="navbar-text">Miembros:</p></li>
+        		  <li><a href="#"><span class="glyphicon glyphicon-user"></span> Registrarse</a></li>
+        		  <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Iniciar sesión</a></li>
     		    </ul>
     	  </div>
     	</nav>
+			</br>
 			<div class="centro">
