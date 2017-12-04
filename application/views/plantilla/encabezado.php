980c192a9f2a416d51217c7bf990a794b47bf969
Freddy R <frondonfermin@gmail.com>
2017-12-04 18:12:13 -0400

    Añadí encabezado y pie de pagina

---
+++ application/views/plantilla/encabezado.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Problemas viales</title>
+		<style>
+			.centro{
+				min-height: 300px;
+			}
+		</style>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	</head>
+	<body>
+		<div class = "container">
+      <nav class="navbar navbar-inverse">
+    	  <div class="container-fluid">
+    		    <div class="navbar-header">
+    		        <a class="navbar-brand" href="#">Equipos deportivos</a>
+    		    </div>
+    		    <ul class="nav navbar-nav">
+        		  <li class="active"><a href="#">Home</a></li>
+        		  <li><a href="#">Page 1</a></li>
+        		  <li><a href="#">Page 2</a></li>
+    		    </ul>
+    		    <ul class="nav navbar-nav navbar-right">
+        		  <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        		  <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+    		    </ul>
+    	  </div>
+    	</nav>
+			<div class="centro">
