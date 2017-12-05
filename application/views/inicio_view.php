938c7d60c18195be127e7a8e88a710b9c17c7906
freilynstr <freilynstr@hotmail.es>
2017-12-05 15:27:36 -0400

    Intento de Front End del Inicio mas chulo

--- application/views/inicio_view.php
+++ application/views/inicio_view.php
@@ -1,52 +1,65 @@
-<div class="container">
-  <table class="table">
-    <thead>
-      <tr>
-        <th>Artículo</th>
-        <th>Descripción</th>
-      </tr>
-    </thead>
+<div class="container ">
+  <div class="col col-sm-12">
+    <div class="col col-sm-8">
+      <table class="table table-striped">
+        <thead>
+          <tr></tr>
+        </thead>
 
-    <tbody>
-      <tr>
-        <td>Artículo 1</td>
-        <td>
-          Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
-          Artículo 1 Aquí irá la descripción del Artículo 1 Aquí irá la
-          descripción del Artículo 1 Aquí irá la descripción del Artículo 1.
-        </td>
-      </tr>
-      <tr>
-        <td>Artículo 2</td>
-        <td>
-          Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
-          Artículo 1 Aquí irá la descripción del Artículo 1 Aquí irá la
-          descripción del Artículo 1 Aquí irá la descripción del Artículo 1.
-        </td>
-      </tr>
-      <tr>
-        <td>Artículo 3</td>
-        <td>
-          Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
-          Artículo 1 Aquí irá la descripción del Artículo 1 Aquí irá la
-          descripción del Artículo 1 Aquí irá la descripción del Artículo 1.
-        </td>
-      </tr>
-      <tr>
-        <td>Artículo 4</td>
-        <td>
-          Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
-          Artículo 1 Aquí irá la descripción del Artículo 1 Aquí irá la
-          descripción del Artículo 1 Aquí irá la descripción del Artículo 1.
-        </td>
-      </tr>
-      <tr>
-        <td>Artículo 5</td>
-        <td>
-          Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
-          Artículo 1 Aquí irá la descripción del Artículo 1 Aquí irá la
-          descripción del Artículo 1 Aquí irá la descripción del Artículo 1.
-        </td>
+        <tbody>
+          <tr>
+            <td>
+              <div class="jumbotron">
+                <h1>Artículo 1</h1>
+                <p>Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
+                Artículo 1 Aquí irá la descripción del Artículo 1 Aquí irá la
+                descripción del Artículo 1 Aquí irá la descripción del Artículo 1.</p>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div class="jumbotron">
+                <h1>Artículo 2</h1>
+                <p>Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
+                Artículo 1 Aquí irá la descripción del Artículo 1 Aquí irá la
+                descripción del Artículo 1 Aquí irá la descripción del Artículo 1.</p>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div class="jumbotron">
+                <h1>Artículo 3</h1>
+                <p>Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
+                Artículo 1 Aquí irá la descripción del Artículo 1 Aquí irá la
+                descripción del Artículo 1 Aquí irá la descripción del Artículo 1.</p>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div class="jumbotron">
+                <h1>Artículo 4</h1>
+                <p>Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
+                Artículo 1 Aquí irá la descripción del Artículo 1 Aquí irá la
+                descripción del Artículo 1 Aquí irá la descripción del Artículo 1.</p>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <div class="jumbotron">
+                <h1>Artículo 5</h1>
+                <p>Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
+                Artículo 1 Aquí irá la descripción del Artículo 1 Aquí irá la
+                descripción del Artículo 1 Aquí irá la descripción del Artículo 1.</p>
+              </div>
+            </td>
+          </tr>
+    </div>
+  </div>
+
       </tr>
     </tbody>
   </table>
