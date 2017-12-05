20272a193fa2582b83b1eb0dfeb9575118885dec
freilynstr <freilynstr@hotmail.es>
2017-12-05 19:11:58 -0400

    Faltante position fixed

--- application/views/inicio_view.php
+++ application/views/inicio_view.php
@@ -1,72 +1,117 @@
 <div class="container">
-  <div class="row">
-    <div class="col-md-8">
-      <table class="table table-striped">
-        <thead>
-          <tr></tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>
-              <div class="jumbotron">
-                <h1>Artículo 1</h1>
-                <p>Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
-                  Artículo 1 Aquí irá la descripción del Artículo 1 Aquí irá la
-                  descripción del Artículo 1 Aquí irá la descripción del Artículo 1.</p>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <div class="jumbotron">
-                <h1>Artículo 2</h1>
-                <p>Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
+  <div class="col">
+    <div class="row">
+      <div class="col-xs-10">
+        <table class="table table-responsive">
+          <thead>
+            <tr></tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <div class="jumbotron">
+                  <h1>Artículo 1</h1>
+                  <p>Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
                     Artículo 1 Aquí irá la descripción del Artículo 1 Aquí irá la
                     descripción del Artículo 1 Aquí irá la descripción del Artículo 1.</p>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <div class="jumbotron">
-                <h1>Artículo 3</h1>
-                <p>Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div class="jumbotron">
+                  <h1>Artículo 2</h1>
+                  <p>Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
                       Artículo 1 Aquí irá la descripción del Artículo 1 Aquí irá la
                       descripción del Artículo 1 Aquí irá la descripción del Artículo 1.</p>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <div class="jumbotron">
-                <h1>Artículo 4</h1>
-                <p>Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div class="jumbotron">
+                  <h1>Artículo 3</h1>
+                  <p>Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
                         Artículo 1 Aquí irá la descripción del Artículo 1 Aquí irá la
                         descripción del Artículo 1 Aquí irá la descripción del Artículo 1.</p>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <div class="jumbotron">
-                <h1>Artículo 5</h1>
-                <p>Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div class="jumbotron">
+                  <h1>Artículo 4</h1>
+                  <p>Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
                           Artículo 1 Aquí irá la descripción del Artículo 1 Aquí irá la
                           descripción del Artículo 1 Aquí irá la descripción del Artículo 1.</p>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <div class="col-md-4 col-md-offset-10">
-      <table class="table table-striped">
-        <thead>
-          <tr>Eventos más cercanos</tr>
-        </thead>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <div class="jumbotron">
+                  <h1>Artículo 5</h1>
+                  <p>Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
+                            Artículo 1 Aquí irá la descripción del Artículo 1 Aquí irá la
+                            descripción del Artículo 1 Aquí irá la descripción del Artículo 1.</p>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
 
-        <tbody>
-      </table>
+      <div class="col-xs-2 col-xs-offset-0">
+        <div class="row">
+          <table class="table table-bordered">
+            <thead>
+              <tr>Eventos cercanos</tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Evento Validación</td>
+              </tr>
+              <tr>
+                <td>Evento Clasificatoria</td>
+              </tr>
+              <tr>
+                <td>Evento Semifinal</td>
+              </tr>
+              <tr>
+                <td>Evento Final</td>
+              </tr>
+              <tr>
+                <td>Evento Fiesta</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="row">
+          <table class="table table-bordered">
+            <thead>
+              <tr>Anuncios Clasificados</tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Gatorade</td>
+              </tr>
+              <tr>
+                <td>Cool Heaven</td>
+              </tr>
+              <tr>
+                <td>Coca Cola</td>
+              </tr>
+              <tr>
+                <td>Clorets</td>
+              </tr>
+              <tr>
+                <td>Memerize</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+      </div>
     </div>
   </div>
 </div>
