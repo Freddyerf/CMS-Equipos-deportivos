8f769afd03b11c95dd491ab2a62b1caccdb5b197
freilynstr <freilynstr@hotmail.es>
2017-12-05 15:15:47 -0400

    Probando con el FronEnd de Inicio

--- application/views/inicio_view.php
+++ application/views/inicio_view.php
@@ -1,2 +1,57 @@
-<?php 
-echo "inicio vista";
+<div class="container">
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Artículo</th>
+        <th>Descripción</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr>
+        <td>Artículo 1</td>
+        <td>
+          Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
+          Artículo 1 Aquí irá la descripción del Artículo 1 Aquí irá la
+          descripción del Artículo 1 Aquí irá la descripción del Artículo 1.
+        </td>
+      </tr>
+      <tr>
+        <td>Artículo 2</td>
+        <td>
+          Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
+          Artículo 1 Aquí irá la descripción del Artículo 1 Aquí irá la
+          descripción del Artículo 1 Aquí irá la descripción del Artículo 1.
+        </td>
+      </tr>
+      <tr>
+        <td>Artículo 3</td>
+        <td>
+          Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
+          Artículo 1 Aquí irá la descripción del Artículo 1 Aquí irá la
+          descripción del Artículo 1 Aquí irá la descripción del Artículo 1.
+        </td>
+      </tr>
+      <tr>
+        <td>Artículo 4</td>
+        <td>
+          Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
+          Artículo 1 Aquí irá la descripción del Artículo 1 Aquí irá la
+          descripción del Artículo 1 Aquí irá la descripción del Artículo 1.
+        </td>
+      </tr>
+      <tr>
+        <td>Artículo 5</td>
+        <td>
+          Aquí irá la descripción del Artículo 1 Aquí irá la descripción del
+          Artículo 1 Aquí irá la descripción del Artículo 1 Aquí irá la
+          descripción del Artículo 1 Aquí irá la descripción del Artículo 1.
+        </td>
+      </tr>
+    </tbody>
+  </table>
+
+  <?php
+  echo "inicio vista";
+  ?>
+</div>
