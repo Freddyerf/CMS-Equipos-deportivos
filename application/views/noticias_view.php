30fa0abae39b0f8b7b65d60a7a1cafab06aa726d
freilynstr <freilynstr@hotmail.es>
2017-12-05 21:10:26 -0400

    Empece con Noticias

---
+++ application/views/noticias_view.php
@@ -0,0 +1,19 @@
+<section class="posts col-md-9">
+  <div class="miga-de-pan">
+    <ol class="breadcrumb">
+      <li><a href="#">Noticias</a></li>
+    </ol>
+    <article class="post clearfix">
+      <a href="#" class="thumb pull-left">
+          <img src="<?php echo base_url() ."images/noticia1.jpg"; ?>" alt="">
+          </a>
+      <h2 class="post-title"><a href="#">Licey arremete contra las águilas por quita vez consecutiva.</a></h2>
+      <p><span class="post-fecha">5 de diciembre del 2017 </span> por <span class="post-autor">Freilyn Rondon</span></p>
+      <p class="post-contenido text-justify">
+         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis provident officiis, reprehenderit numquam.
+         Praesentium veritatis eos tenetur magni debitis inventore fugit, magnam, reiciendis, saepe obcaecati ex vero
+         quaerat distinctio velit.
+      </p>
+    </article>
+  </div>
+</section>
