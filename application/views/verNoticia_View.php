68e046fddb001c47109518063101e9bc2ccdb09b
freilynstr <freilynstr@hotmail.es>
2017-12-06 11:05:29 -0400

    Empece con la Vista por noticia

---
+++ application/views/verNoticia_View.php
@@ -0,0 +1,12 @@
+<div class="container">
+  <div class="row">
+    <div class="col col-sm-12">
+      <div class="col col-sm-8">
+        <h1>Hola Mundo</h1>
+      </div>
+      <div class="col-sm-4 col-sm-offset-0">
+        <h1>Hola Persona!</h1>
+      </div>
+    </div>
+  </div>
+</div>
